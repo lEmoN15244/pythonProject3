61661cf5ec07e876df06aa5311f3fa4774c5288e
Alex <nfqaey.000@gmail.com>
2024-07-09 10:42:34 +0300

    добавление файла запуска (main)

--- src/main.py
+++ src/main.py
@@ -1,19 +1,3 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
-user_card_number = input("Введите номер карты ")
-
-if len(user_card_number) != 16:
-    print("Введен некорректный номер карты ")
-else:
-    card_number: str = user_card_number
-
-user_account_number = input("Введите номер счета")
-if len(user_account_number) != 20:
-    print("Введен некорректный номер счета")
-else:
-    account: str = str(user_account_number)
-
-result_mask_card = get_mask_card_number(card_number)
-result_mask_account = get_mask_account(account)
-print(result_mask_card)
-print(result_mask_account)
+print(mask_account_card())
