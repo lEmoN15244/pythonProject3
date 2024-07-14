fc1e4ce7d88d0df2b73a5e8f73495c3476a5b2fc
Alex <nfqaey.000@gmail.com>
2024-07-14 21:31:07 +0300

    добавление readme и изменение main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from src.widget import get_date, mask_account_card
 
-user_date = input("Введите дату")
-print(mask_account_card())
-print(get_date(user_date))
+from src.processing import filter_by_state, sort_by_date
+
+user_input = input("Введите данные")
+# выбери нужную функцию и расскомить
+# print(mask_account_card(user_input))
+# print(get_date(user_input))
+# print(filter_by_state(user_input))
