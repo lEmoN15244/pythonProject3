ff2de64dbc3f38968335f8f2db82d0419eeeabc1
Alex <nfqaey.000@gmail.com>
2024-07-16 19:23:08 +0300

    Переделка файла main, апгрейд readme

--- src/main.py
+++ src/main.py
@@ -2,8 +2,20 @@ from src.widget import get_date, mask_account_card
 
 from src.processing import filter_by_state, sort_by_date
 
-user_input = input("Введите данные")
-# выбери нужную функцию и расскомить
-# print(mask_account_card(user_input))
+operations = []
+user_list_operation =[]
+number = ""
+user_input = input("Введите какую операцию хотите выполнить? (максировка счета или карты/ сортировка операций/сортировка операций по дате)")
+if user_input == "максировка счета или карты":
+    print(mask_account_card())
+elif user_input == "сортировка операций по дате":
+    user_list_operation = list(input("Введите список операций"))
+    print(sort_by_date(user_list_operation))
+elif user_input == "сортировка операций":
+    operations == list(input("Введите список операций"))
+    print(filter_by_state(operations))
+
+
+
 # print(get_date(user_input))
-# print(filter_by_state(user_input))
+
