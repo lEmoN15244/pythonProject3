6d8aa81308e50abeea5dd16df275698ba0069226
lEmoN15244 <nfqaey.000@gmail.com>
2024-07-20 22:29:46 +0300

    Revert "исправлеение функций processing и форматирование"

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
+from src.widget import get_date, mask_account_card
+
 from src.processing import filter_by_state, sort_by_date
-from src.widget import mask_account_card
 
 operations = []
-user_list_operation = []
+user_list_operation =[]
 number = ""
-user_input = input(
-    """Введите какую операцию хотите выполнить?
-    (максировка счета или карты/ сортировка операций/сортировка операций по дате)"""
-)
+user_input = input("Введите какую операцию хотите выполнить? (максировка счета или карты/ сортировка операций/сортировка операций по дате)")
 if user_input == "максировка счета или карты":
     print(mask_account_card())
 elif user_input == "сортировка операций по дате":
@@ -18,4 +16,6 @@ elif user_input == "сортировка операций":
     print(filter_by_state(operations))
 
 
+
 # print(get_date(user_input))
+
--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(user_input) -> str:
+def mask_account_card() -> str:
     """Функция выделяющая номер карты/аккаунта"""
     user_input_number = ""
     name_card_account = []
+    user_input = input("Введите номер счета или карты")
     user_input_splited = user_input.split()
     for part_input in user_input_splited:
-        if part_input.isdigit():
+        if part_input.isdigit() == True:
             user_input_number = part_input
         else:
             name_card_account.append(part_input)
@@ -15,24 +16,15 @@ def mask_account_card(user_input) -> str:
         name_card_account.append(get_mask_account(user_input_number))
     elif len(user_input_number) == 16:
         name_card_account.append(get_mask_card_number(user_input_number))
-    else:
-        return "Введите корректный номер счета"
 
     result = " ".join(name_card_account)
     return result
 
 
 def get_date(user_date: str) -> str:
-
-    if len(user_date) >= 10:
-        index_time = user_date.find("T")
-        date = user_date[:index_time]
-        year_month_day = date.split("-")
-        year_month_day.reverse()
-        date_right = ".".join(year_month_day)
-        return date_right
-    else:
-        return "Введите корректную дату"
-
-
-# print(get_date("2024-03-11T02:26:18.671407"))
+    index_time = user_date.find("T")
+    date = user_date[:index_time]
+    year_month_day = date.split("-")
+    year_month_day.reverse()
+    date_right = ".".join(year_month_day)
+    return date_right
