272890ca2403286cf983fdb761d2b535c12030b7
Alex <nfqaey.000@gmail.com>
2024-07-26 14:14:30 +0300

    Создание тестов к функциям generators, апгрейд Readme

--- src/generators.py
+++ src/generators.py
@@ -1,52 +1,102 @@
 import random
 import textwrap
-from random import randint
+# from random import randint
 
-transactions = [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572", "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}}, "description": "Перевод организации", "from": "Счет 75106830613657916952", "to": "Счет 11776614605963066702"},
-      {
-              "id": 142264268,
-              "state": "EXECUTED",
-              "date": "2019-04-04T23:20:05.206878",
-              "operationAmount": {
-                  "amount": "79114.93",
-                  "currency": {
-                      "name": "USD",
-                      "code": "USD"
-                  }
-              },
-              "description": "Перевод со счета на счет",
-              "from": "Счет 19708645243227258542",
-              "to": "Счет 75651667383060284188"
-       }]
+# transactions = [
+#         {
+#             "id": 939719570,
+#             "state": "EXECUTED",
+#             "date": "2018-06-30T02:08:58.425572",
+#             "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод организации",
+#             "from": "Счет 75106830613657916952",
+#             "to": "Счет 11776614605963066702",
+#         },
+#         {
+#             "id": 142264268,
+#             "state": "EXECUTED",
+#             "date": "2019-04-04T23:20:05.206878",
+#             "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод со счета на счет",
+#             "from": "Счет 19708645243227258542",
+#             "to": "Счет 75651667383060284188",
+#         },
+#         {
+#             "id": 873106923,
+#             "state": "EXECUTED",
+#             "date": "2019-03-23T01:09:46.296404",
+#             "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод со счета на счет",
+#             "from": "Счет 44812258784861134719",
+#             "to": "Счет 74489636417521191160",
+#         },
+#         {
+#             "id": 895315941,
+#             "state": "EXECUTED",
+#             "date": "2018-08-19T04:27:37.904916",
+#             "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод с карты на карту",
+#             "from": "Visa Classic 6831982476737658",
+#             "to": "Visa Platinum 8990922113665229",
+#         },
+#         {
+#             "id": 594226727,
+#             "state": "CANCELED",
+#             "date": "2018-09-12T21:27:25.241689",
+#             "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод организации",
+#             "from": "Visa Platinum 1246377376343588",
+#             "to": "Счет 14211924144426031657",
+#         },
+#     ]
+
 
 def filter_by_currency(transactions, currency_name="USD"):
     """Функция выдает транзакции, где операция соответсвует заданному параметру"""
     for transaction in transactions:
         if transaction["operationAmount"]["currency"]["name"] == currency_name:
-             yield transaction
-
+            yield transaction
 
 
 # usd_transactions = filter_by_currency(transactions, "USD")
 # for i in range(2):
 #     print(next(usd_transactions))
 
+
 def transaction_descriptions(transactions):
     """Генератор выдающий описание каждой операции по очереди"""
     for transaction in transactions:
-        yield transaction['description']
+        yield transaction["description"]
+
 
 # descriptions = transaction_descriptions(transactions)
 # for i in range(2):
 #     print(next(descriptions))
 
 
-def card_number_generator():
+def card_number_generator_random():
+    """ Функция генерирующая случайный номер карты"""
     while True:
         number = str(random.randint(0000000000000000, 9999999999999999))
         card_number = textwrap.wrap(number, 4)
         result_card_number = " ".join(card_number)
-        yield card_number
+        yield result_card_number
 
+# def card_number_generator(start , end):
+#     number = str(x for x in range(start,end))
+#     card_number = textwrap.wrap(number, 4)
+#     result_card_number = " ".join(card_number)
+#     yield number
+#
+# print(next(card_number_generator(0000000000000000 , 9999999999999999)))
 
+def card_number_generator(start, stop):
+    """Генератор номеров карты по порядку в заданном диапазоне"""
+    if start > 0 and stop < 9999999999999999:
+        for number in range(start, stop):
+            card_number = "0"*(16-len(str(number))) + str(number)
+            card_number_result = " ".join(textwrap.wrap(card_number, 4))
+            yield card_number_result
 
+# for card_number in card_number_generator(1, 5):
+#     print(card_number)
---
+++ tests/test_generators.py
@@ -0,0 +1,46 @@
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+
+
+def test_filter_by_currency(transaction_list):
+    usd_transactions = filter_by_currency(transaction_list, "USD")
+    assert next(usd_transactions) == {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
+    assert next(usd_transactions) == {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+    assert next(usd_transactions) == {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    }
+
+
+def test_transaction_description(transaction_list):
+    descriptions = transaction_descriptions(transaction_list)
+    assert next(descriptions) == "Перевод организации"
+    assert next(descriptions) == "Перевод со счета на счет"
+    assert next(descriptions) == "Перевод со счета на счет"
+
+
+def test_card_number_generator():
+    card_numbers = card_number_generator(5, 8)
+    assert next(card_numbers) == "0000 0000 0000 0005"
+    assert next(card_numbers) == "0000 0000 0000 0006"
+    assert next(card_numbers) == "0000 0000 0000 0007"
